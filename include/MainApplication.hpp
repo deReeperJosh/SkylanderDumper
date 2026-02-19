7b5f83ae4990ce22a9827623cc823304af89a6b6
Joshua de Reeper <joshua@dereeper.co.nz>
2026-02-19 14:44:29 +0100

    plutonium and format

--- include/MainApplication.hpp
+++ include/MainApplication.hpp
@@ -5,32 +5,51 @@
 #include <pu/Plutonium>
 
 // Define your main layout as a class inheriting from pu::Layout
-class CustomLayout : public pu::ui::Layout {
-    private:
+class CustomLayout : public pu::ui::Layout
+{
+private:
+  // An easy way to keep objects is to have them as private members
+  // Using ::Ref (of a Plutonium built-in object or any class having PU_SMART_CTOR) is an alias to a
+  // shared_ptr of the instance.
+  pu::ui::elm::TextBlock::Ref helloText;
 
-        // An easy way to keep objects is to have them as private members
-        // Using ::Ref (of a Plutonium built-in object or any class having PU_SMART_CTOR) is an alias to a shared_ptr of the instance.
-        pu::ui::elm::TextBlock::Ref helloText;
+public:
+  CustomLayout();
 
-    public:
+  // Have ::Ref alias and ::New() static constructor
+  PU_SMART_CTOR(CustomLayout)
+};
 
-        CustomLayout();
+class NFCLayout : public pu::ui::Layout
+{
+private:
+  pu::ui::elm::Menu::Ref options_menu;
+  pu::ui::elm::MenuItem::Ref explore_menu_item;
+  pu::ui::elm::MenuItem::Ref app_list_menu_item;
+  pu::ui::elm::MenuItem::Ref tickets_menu_item;
+  pu::ui::elm::MenuItem::Ref web_browser_menu_item;
+  pu::ui::elm::MenuItem::Ref account_menu_item;
+  pu::ui::elm::MenuItem::Ref amiibo_menu_item;
+  pu::ui::elm::MenuItem::Ref settings_menu_item;
+  pu::ui::elm::MenuItem::Ref about_menu_item;
 
-        // Have ::Ref alias and ::New() static constructor
-        PU_SMART_CTOR(CustomLayout)
+public:
+  NFCLayout();
+
+  PU_SMART_CTOR(NFCLayout)
 };
 
 // Define your application (can't instantiate base class, so need to make a derived one)
-class MainApplication : public pu::ui::Application {
-    private:
+class MainApplication : public pu::ui::Application
+{
+private:
+  // Layout instance
+  CustomLayout::Ref layout;
 
-        // Layout instance
-        CustomLayout::Ref layout;
-    
-    public:
-        using Application::Application;
-        PU_SMART_CTOR(MainApplication)
+public:
+  using Application::Application;
+  PU_SMART_CTOR(MainApplication)
 
-        // We need to define this, and use it to initialize everything
-        void OnLoad() override;
+  // We need to define this, and use it to initialize everything
+  void OnLoad() override;
 };
--- source/MainApplication.cpp
+++ source/MainApplication.cpp
@@ -2,52 +2,121 @@
 
 // Implement all the layout/application functions here
 
-CustomLayout::CustomLayout() : Layout::Layout() {
-    // Create the TextBlock instance with the text we want
-    this->helloText = pu::ui::elm::TextBlock::New(300, 300, "Press X to answer my question");
-    
-    // Add the instance to the layout. IMPORTANT! this MUST be done for them to be used, having them as members is not enough (just a simple way to keep them)
-    this->Add(this->helloText);
+CustomLayout::CustomLayout() : Layout::Layout()
+{
+  // Create the TextBlock instance with the text we want
+  this->helloText = pu::ui::elm::TextBlock::New(300, 300, "Press X to answer my question");
+
+  // Add the instance to the layout. IMPORTANT! this MUST be done for them to be used, having them
+  // as members is not enough (just a simple way to keep them)
+  this->Add(this->helloText);
 }
 
-void MainApplication::OnLoad() {
-    // Create the layout (calling the smart constructor above)
-    this->layout = CustomLayout::New();
+NFCLayout::NFCLayout() : Layout::Layout()
+{
+  this->options_menu = pu::ui::elm::Menu::New(
+      0, 280, pu::ui::render::ScreenWidth, g_Settings.GetColorScheme().menu_base,
+      g_Settings.GetColorScheme().menu_base_focus,
+      g_Settings.json_settings.ui.value().menu_item_size.value(),
+      ComputeDefaultMenuItemCount(g_Settings.json_settings.ui.value().menu_item_size.value()));
+  g_Settings.ApplyToMenu(this->options_menu);
+  this->explore_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(277));
+  this->explore_menu_item->SetIcon(GetCommonIcon(CommonIconKind::SdCard));
+  this->explore_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->explore_menu_item->AddOnKey(std::bind(&MainMenuLayout::exploreMenu_DefaultKey, this));
+  this->app_list_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(488));
+  this->app_list_menu_item->SetIcon(GetCommonIcon(CommonIconKind::Game));
+  this->app_list_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->app_list_menu_item->AddOnKey(std::bind(&MainMenuLayout::appList_DefaultKey, this));
+  this->tickets_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(4));
+  this->tickets_menu_item->SetIcon(GetCommonIcon(CommonIconKind::Ticket));
+  this->tickets_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->tickets_menu_item->AddOnKey(std::bind(&MainMenuLayout::tickets_DefaultKey, this));
+  this->web_browser_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(5));
+  this->web_browser_menu_item->SetIcon(GetCommonIcon(CommonIconKind::Browser));
+  this->web_browser_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->web_browser_menu_item->AddOnKey(std::bind(&MainMenuLayout::webBrowser_DefaultKey, this));
+  this->account_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(6));
+  this->account_menu_item->SetIcon(GetCommonIcon(CommonIconKind::Accounts));
+  this->account_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->account_menu_item->AddOnKey(std::bind(&MainMenuLayout::account_DefaultKey, this));
+  this->amiibo_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(283));
+  this->amiibo_menu_item->SetIcon(GetCommonIcon(CommonIconKind::Amiibo));
+  this->amiibo_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->amiibo_menu_item->AddOnKey(std::bind(&MainMenuLayout::amiibo_DefaultKey, this));
+  this->settings_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(375));
+  this->settings_menu_item->SetIcon(GetCommonIcon(CommonIconKind::Settings));
+  this->settings_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->settings_menu_item->AddOnKey(std::bind(&MainMenuLayout::settings_DefaultKey, this));
+  this->about_menu_item = pu::ui::elm::MenuItem::New(cfg::Strings.GetString(8));
+  this->about_menu_item->SetIcon(GetCommonIcon(CommonIconKind::Info));
+  this->about_menu_item->SetColor(g_Settings.GetColorScheme().text);
+//   this->about_menu_item->AddOnKey(std::bind(&MainMenuLayout::about_DefaultKey, this));
+  this->options_menu->AddItem(this->explore_menu_item);
+  this->options_menu->AddItem(this->app_list_menu_item);
+  this->options_menu->AddItem(this->tickets_menu_item);
+  this->options_menu->AddItem(this->web_browser_menu_item);
+  this->options_menu->AddItem(this->account_menu_item);
+  this->options_menu->AddItem(this->amiibo_menu_item);
+  this->options_menu->AddItem(this->settings_menu_item);
+  this->options_menu->AddItem(this->about_menu_item);
+  this->Add(this->options_menu);
+}
 
-    // Load the layout. In applications layouts are loaded, not added into a container (you don't select an added layout, just load it from this function)
-    // Simply explained: loading layout = the application will render that layout in the very next frame
-    this->LoadLayout(this->layout);
+void MainApplication::OnLoad()
+{
+  // Create the layout (calling the smart constructor above)
+  this->layout = CustomLayout::New();
 
-    // Set a function when input is caught. This input handling will be the first one to be handled (before Layout or any Elements)
-    // Using a lambda function here to simplify things
-    // You can use member functions via std::bind() C++ wrapper
-    this->SetOnInput([&](const u64 keys_down, const u64 keys_up, const u64 keys_held, const pu::ui::TouchPoint touch_pos) {
-         // If X is pressed, start with our dialog questions!
-        if(keys_down & HidNpadButton_X) {
-            int opt = this->CreateShowDialog("Question", "Do you like apples?", { "Yes!", "No...", "Cancel" }, true); // (using latest option as cancel option)
-            // -1 and -2 are similar, but if the user cancels manually -1 is set, other types or cancel should be -2.
-            if((opt == -1) || (opt == -2))  {
-                this->CreateShowDialog("Cancel", "Last question was canceled.", { "Ok" }, true); // If we will ignore the option, it doesn't matter if this is true or false
-            }
-            else {
-                // Otherwise, opt will be the index of the options we passed to the dialog
-                switch(opt) {
-                    // "Yes!" was selected
-                    case 0:  {
-                        this->CreateShowDialog("Answer", "Really? I like apples too!", { "Ok" }, true); // Same here ^
-                        break;
-                    }
-                    // "No..." was selected
-                    case 1: {
-                        this->CreateShowDialog("Answer", "Oh, bad news then...", { "Ok" }, true); // And here ^
-                        break;
-                    }
-                }
-            }
+  // Load the layout. In applications layouts are loaded, not added into a container (you don't
+  // select an added layout, just load it from this function) Simply explained: loading layout = the
+  // application will render that layout in the very next frame
+  this->LoadLayout(this->layout);
+
+  // Set a function when input is caught. This input handling will be the first one to be handled
+  // (before Layout or any Elements) Using a lambda function here to simplify things You can use
+  // member functions via std::bind() C++ wrapper
+  this->SetOnInput([&](const u64 keys_down, const u64 keys_up, const u64 keys_held,
+                       const pu::ui::TouchPoint touch_pos) {
+    // If X is pressed, start with our dialog questions!
+    if (keys_down & HidNpadButton_X)
+    {
+      int opt =
+          this->CreateShowDialog("Question", "Do you like apples?", {"Yes!", "No...", "Cancel"},
+                                 true);  // (using latest option as cancel option)
+      // -1 and -2 are similar, but if the user cancels manually -1 is set, other types or cancel
+      // should be -2.
+      if ((opt == -1) || (opt == -2))
+      {
+        this->CreateShowDialog(
+            "Cancel", "Last question was canceled.", {"Ok"},
+            true);  // If we will ignore the option, it doesn't matter if this is true or false
+      }
+      else
+      {
+        // Otherwise, opt will be the index of the options we passed to the dialog
+        switch (opt)
+        {
+        // "Yes!" was selected
+        case 0:
+        {
+          this->CreateShowDialog("Answer", "Really? I like apples too!", {"Ok"},
+                                 true);  // Same here ^
+          break;
         }
-        // If + is pressed, exit application
-        else if(keys_down & HidNpadButton_Plus) {
-            this->Close();
+        // "No..." was selected
+        case 1:
+        {
+          this->CreateShowDialog("Answer", "Oh, bad news then...", {"Ok"}, true);  // And here ^
+          break;
         }
-    });
+        }
+      }
+    }
+    // If + is pressed, exit application
+    else if (keys_down & HidNpadButton_Plus)
+    {
+      this->Close();
+    }
+  });
 }
